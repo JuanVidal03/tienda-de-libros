00360045c8b9aeb5ad0688ebd98d5852d141c320
Juan Vidal <juanmvg2003@gmail.com>
2022-08-10 19:02:15 -0500

    fue creado el carrito y añadido el menu

--- js/carrito.js
+++ js/carrito.js
@@ -2,9 +2,11 @@
 //obtengo mediante ell dom
 const contenedorProductos = document.getElementById("productos-destacados-container");
 
+
+
 //recorrido de array productos, obtengo los valores que necesito para desarrollar los productos
 productos.forEach((producto) =>{
-
+    
     //creo elemento contenedor del producto
     let esquemaProducto = document.createElement("div");
     esquemaProducto.classList.add("card");
@@ -34,9 +36,78 @@ productos.forEach((producto) =>{
 //hago un array de carrito vacio debido a que aún no tengo nigún dato
 let carrito = [];
 
-//función para agregar al carrito
-const agregarCarrrito = (prodId) =>{
-    const item = productos.find((prod) => prod.id === prodId);
-    carrito.push(item);
-    console.log(carrito);
+//para cargarlo en el localk storage
+document.addEventListener("DOMContentLoaded", () =>{
+
+    if(localStorage.getItem("carrito")){
+        carrito = JSON.parse(localStorage.getItem("carrito"))
+        actualizarCarrito();
+    }
+
+});
+
+//aqui se va almacenar los cambios que pasan en el carrito
+const contenedorCarrito = document.getElementById("carrito-contenedor");
+
+//función que actualiza carrito
+const actualizarCarrito = () => {
+    //esto hace que los productos no se dupliquen
+    contenedorCarrito.innerHTML = "";
+
+    carrito.forEach((producto) =>{
+
+        const div = document.createElement("div");
+        div.classList.add("producto-en-carrito");
+        div.innerHTML = `<p>${producto.nombre}</p>
+        <p>Precio: ${producto.precio}</p>
+        <img src="../img/compartimiento.png" class="boton-eliminar" onclick="eliminarDelCarrito(${producto.id})"/>`
+        
+        contenedorCarrito.appendChild(div);
+
+        //localstorage setitem
+        localStorage.setItem("carrito", JSON.stringify(carrito));
+    });
+
+    //para que el contador lleve la cuenta de los productos agregados
+    const contadorCarrito = document.getElementById("contador-carrito");
+    contadorCarrito.innerText = carrito.length;
+    //para sumar el precio de los productos
+    const precioTotal = document.getElementById("precioTotal");
+    precioTotal.innerText = carrito.reduce((acumulado, producto) => acumulado + producto.precio, 0);
+
+
 };
+
+
+const eliminarDelCarrito = (id) =>{
+    //encontrar el producto
+    const item = carrito.find((producto) => producto.id === id);
+    const indice = carrito.indexOf(item);
+    //para recortar el producto del array
+    carrito.splice(indice, 1);
+
+    //se llama a la función para actualizar el carrito
+    actualizarCarrito();
+};
+
+
+
+//función para agregar al carrito
+const agregarCarrrito = (id) =>{
+
+    const item = productos.find((producto) => producto.id === id);
+    carrito.push(item);
+    //llamamos funcion que actualiza carrito
+    actualizarCarrito();
+};
+
+
+//boton para vaciar carritoen el DOM
+const btnVaciarCarrito = document.getElementById("vaciar-carrito");
+
+//evento para vaciar el carrito
+btnVaciarCarrito.addEventListener("click", () =>{
+    carrito.length = 0;
+    localStorage.clear();
+    actualizarCarrito();
+});
